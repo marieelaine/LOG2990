184cb2da59c7299435c04bc79ec15aabcd782980
Olivier Naud-Dulude <olivier.naud-dulude@hotmail.com>
2018-09-07 16:03:26 -0400

    Début de la template pour créer la liste des parties (image et nom seulement)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BasicService } from "./basic.service";
 import { HttpClientModule } from "@angular/common/http";
+import { ListePartiesComponent } from './liste-parties/liste-parties.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListePartiesComponent
   ],
   imports: [
     BrowserModule,
