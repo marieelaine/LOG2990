712e25ed2f9963fd3cac0852df72ba0e06915be1
marieelaine <marieelaine@github.com>
2018-09-07 13:45:28 -0400

    added the html header on the main page

--- client/src/app/common/header/header.component.ts
+++ client/src/app/common/header/header.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public constructor() { }
 
-  ngOnInit() {
+  // tslint:disable-next-line:typedef
+  public ngOnInit() {
   }
 
 }
