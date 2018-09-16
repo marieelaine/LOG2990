2da49e01587e483b2c8ee0a10cd5267ea6f7adad
Olivier Naud-Dulude <olivier.naud-dulude@hotmail.com>
2018-09-16 13:10:08 -0400

    Revert "avancement creation partie admin"
    This reverts commit d16cb486fd972e012a9ee32311fd242ad506a37d.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
-import { RouterModule, Routes } from "@angular/router";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDividerModule } from '@angular/material/divider';
-
-import { ModalModule } from "ngx-bootstrap";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppComponent } from "./app.component";
 import { BasicService } from "./basic.service";
-import { ListePartiesComponent } from "./liste-parties/liste-parties.component";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Routes } from "@angular/router";
+import { ListePartiesComponent } from './liste-parties/liste-parties.component';
+
 import { HeaderComponent } from "./common/header/header.component";
 import { VueInitialeComponent } from "./vue-initiale/vue-initiale.component";
 import { AdminComponent } from "./admin/admin.component";
+
+import { ModalModule } from 'ngx-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { NgbdModalConfig } from './modal-config';
 
 const appRoutes: Routes = [
   { path: "", component: VueInitialeComponent },
@@ -44,8 +44,6 @@ const appRoutes: Routes = [
     FormsModule,
     ReactiveFormsModule,
     NgbModule.forRoot(),
-    BrowserAnimationsModule,
-    MatDividerModule,
   ],
   providers: [BasicService],
   bootstrap: [AppComponent],
