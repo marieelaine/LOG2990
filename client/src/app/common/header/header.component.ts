324159389d2fd062f49684741b09153f77a71a66
Olivier Naud-Dulude <olivier.naud-dulude@hotmail.com>
2018-09-19 12:15:47 -0400

    Changement du nom sur le header click amene à liste-parties

--- client/src/app/common/header/header.component.ts
+++ client/src/app/common/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-header",
@@ -7,10 +8,14 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HeaderComponent implements OnInit {
 
-  public constructor() { }
+  public constructor(private router: Router) { }
 
   // tslint:disable-next-line:typedef
   public ngOnInit() {
+  }
+
+  public OnHeaderTitleClick() {
+    this.router.navigateByUrl("/liste-parties");
   }
 
 }
