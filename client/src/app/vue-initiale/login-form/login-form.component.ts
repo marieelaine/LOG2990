7c0b5603e7da8cd24ee125ffc109badd48dec016
Charles-Auguste Marois <charles.marois@polymtl.ca>
2018-09-22 18:07:19 -0400

    Creation UserService
    Utile pour recuperer les erreurs en backend pour le loginform

--- client/src/app/vue-initiale/login-form/login-form.component.ts
+++ client/src/app/vue-initiale/login-form/login-form.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { User} from "../login-form/user";
 import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
+import { UserService } from "../user.service";
 declare var particlesJS: any;
 
 export const USER_URL: string = "http://localhost:3000/users/";
@@ -11,6 +12,7 @@ const URL_AJOUTER_PISTE: string = USER_URL + "ajouter/";
 
 @Component({
   selector: "app-login-form",
+  providers: [UserService],
   templateUrl: "./login-form.component.html",
   styleUrls: ["./login-form.component.css"]
 })
@@ -20,14 +22,17 @@ export class LoginFormComponent implements OnInit {
   public BASE_URL: string = "http://localhost:3000/";
   public loginForm: FormGroup;
 
-  public constructor(private router: Router, private http: HttpClient) {
+  public constructor(private router: Router, private http: HttpClient, private userService: UserService) {
     this.loginForm = this.createFormGroup();
   }
 
   public onSubmit(): void {
     const result: User = Object.assign({}, this.loginForm.value);
-    this.creerNouveauUser(result);
-    this.router.navigateByUrl("/liste-parties");
+    this.userService.register(JSON.stringify(this.loginForm.value))
+    .subscribe(
+    (data) => {console.log(data); this.router.navigate(["/liste-parties"]); },
+    (error) => console.error(error)
+    );
  }
 
   public createFormGroup(): FormGroup {
@@ -43,8 +48,12 @@ export class LoginFormComponent implements OnInit {
     particlesJS.load("particles-js", "assets/particles.json", null);
   }
 
-  public obtenirUser(identifiant: string): Observable<User> {
+  public obtenirUserId(identifiant: string): Observable<User> {
     return this.http.get<User>(USER_URL + identifiant);
+  }
+
+  public obtenirUserName(username: string): Observable<User> {
+    return this.http.get<User>(USER_URL + username);
   }
 
   public async creerNouveauUser(user: User): Promise<Object> {
---
+++ client/src/app/vue-initiale/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+@Injectable()
+export class UserService {
+
+  constructor(private _http: HttpClient) { }
+
+  register(body: any) {
+    return this._http.post("http://127.0.0.1:3000/users/ajouter", body, {
+      observe: "body",
+      headers: new HttpHeaders().append("Content-Type", "application/json")
+    });
+  }
+}
