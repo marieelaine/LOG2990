26328099673751589baba8f04be5626567034c04
Olivier Naud-Dulude <olivier.naud-dulude@hotmail.com>
2018-09-24 10:16:26 -0400

    fix bug

--- client/src/app/liste-parties/liste-parties.component.ts
+++ client/src/app/liste-parties/liste-parties.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PartieSimpleInterface } from './partie-simple/partie-simple.component';
-import { PartieMultipleComponent, PartieMultipleInterface } from './partie-multiple/partie-multiple.component';
+import { PartieMultipleInterface } from './partie-multiple/partie-multiple.component';
 
 @Component({
   selector: 'app-liste-parties',
@@ -10,7 +10,7 @@ import { PartieMultipleComponent, PartieMultipleInterface } from './partie-multi
 export class ListePartiesComponent implements OnInit {
 
   partieSimpleDiv: HTMLElement;
-  listePartiesSimples: PartieSimpleComponent[] = [
+  listePartiesSimples: PartieSimpleInterface[] = [
     { title: 'Nissan Patrol', imagePath: 'assets/NissanPatrol.bmp', isElevatedActive: false,
       timesSolo: [320, 500], timesOneVsOne: [],
       },
@@ -18,7 +18,7 @@ export class ListePartiesComponent implements OnInit {
       timesSolo: [550, 302, 419, 3141], timesOneVsOne: [41241, 412, 52, 5235, 552],
       }  ];
 
-  listePartiesMultiples: PartieMultipleComponent[] = [
+  listePartiesMultiples: PartieMultipleInterface[] = [
     { title: 'Mona Lisa', imagePath: 'assets/monaLisa.bmp', isElevatedActive: false,
       timesSolo: [312, 415, 6462, 1], timesOneVsOne: [312, 3],
     }  ];
