6a503a1dd6a3ef8a06a5dde4d892c42ab2923dfe
marieelaine <marieelaine@github.com>
2018-09-23 20:11:40 -0400

    added imports for the liste-partie component: ready for testing

--- client/src/app/liste-parties/partie-simple/partie-simple.component.spec.ts
+++ client/src/app/liste-parties/partie-simple/partie-simple.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PartieSimpleComponent } from './partie-simple.component';
+import { MatCardModule } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('PartieSimpleComponent', () => {
   let component: PartieSimpleComponent;
@@ -8,7 +10,8 @@ describe('PartieSimpleComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ PartieSimpleComponent ]
+      declarations: [ PartieSimpleComponent ],
+      imports: [MatCardModule, RouterTestingModule]
     })
     .compileComponents();
   });
