10edda2eb95a34815f92f3c63443887c0e6b070e
marieelaine <marieelaine@github.com>
2018-09-23 20:17:59 -0400

    added imports for the vue-initiale component: ALL components ready for testing

--- client/src/app/vue-initiale/user.service.spec.ts
+++ client/src/app/vue-initiale/user.service.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, inject } from "@angular/core/testing";
 
 import { UserService } from "../vue-initiale/user.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe("UserService", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [UserService]
+      providers: [UserService],
+      imports: [HttpClientTestingModule]
     });
   });
 
--- client/src/app/vue-initiale/vue-initiale.component.spec.ts
+++ client/src/app/vue-initiale/vue-initiale.component.spec.ts
@@ -3,6 +3,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { VueInitialeComponent } from './vue-initiale.component';
 import { HeaderComponent } from '../common/header/header.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { MatToolbarModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('VueInitialeComponent', () => {
   let component: VueInitialeComponent;
@@ -10,7 +15,8 @@ describe('VueInitialeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VueInitialeComponent, HeaderComponent, LoginFormComponent ]
+      declarations: [ VueInitialeComponent, HeaderComponent, LoginFormComponent ],
+      imports: [MatToolbarModule, FormsModule, ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule]
     })
     .compileComponents();
   }));
