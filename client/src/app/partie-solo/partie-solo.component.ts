26afbb5e4bbbdf99911a861822722bed68fec2fc
Bernard Meunier <bern97@hotmail.ca>
2018-09-23 22:12:26 -0400

    partieSolo

--- client/src/app/partie-solo/partie-solo.component.ts
+++ client/src/app/partie-solo/partie-solo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { element } from '@angular/core/src/render3/instructions';
+import { ChronoComponent } from "../chrono/chrono.component"
 
 @Component({
   selector: 'app-partie-solo',
@@ -7,8 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PartieSoloComponent implements OnInit {
 
+  blur: boolean = true;
+  chrono: ChronoComponent;
+  timer: number = 0;
+  
+
   constructor() { }
 
+  start(){
+    this.blur = false;
+    let button = document.getElementById("StartButton");
+    button.remove();
+  }
   ngOnInit() {
   }
 
