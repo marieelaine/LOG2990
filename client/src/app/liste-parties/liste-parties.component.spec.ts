483805dd054c51886574edaf31acc3605906118d
Charles-Auguste Marois <charles.marois@polymtl.ca>
2018-09-26 00:27:46 -0400

    Test routing partie-solo de liste-parties

--- client/src/app/liste-parties/liste-parties.component.spec.ts
+++ client/src/app/liste-parties/liste-parties.component.spec.ts
@@ -1,27 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Location } from "@angular/common";
 import { ListePartiesComponent } from './liste-parties.component';
 import { PartieSimpleComponent } from './partie-simple/partie-simple.component';
 import { PartieMultipleComponent } from './partie-multiple/partie-multiple.component';
 import { MatCardHeader, MatCardModule } from '@angular/material';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { PartieSoloComponent } from "../partie-solo/partie-solo.component";
+
 describe('ListePartiesComponent', () => {
   let component: ListePartiesComponent;
   let fixture: ComponentFixture<ListePartiesComponent>;
+  let location: Location;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListePartiesComponent, PartieSimpleComponent, PartieMultipleComponent ],
+      declarations: [ ListePartiesComponent, PartieSimpleComponent, PartieMultipleComponent, PartieSoloComponent ],
       schemas: [
         CUSTOM_ELEMENTS_SCHEMA
       ],
-      imports: [MatCardModule, RouterTestingModule ]
+      imports: [MatCardModule, RouterTestingModule.withRoutes([
+        { path: "liste-parties", component: ListePartiesComponent },
+        { path: "partie-solo", component: PartieSoloComponent },
+      ]) ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    location = TestBed.get(Location);
     fixture = TestBed.createComponent(ListePartiesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -30,4 +38,16 @@ describe('ListePartiesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('click on button partie-solo redirects you to /partie-solo', fakeAsync(() => {
+    const elem = fixture.debugElement;
+    const button = elem.query((e) => e.name === 'button');
+    expect(!!button).toBe(true);
+    button.nativeElement.click();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(location.path()).toBe('/partie-solo');
+    });
+
+  }));
 });
