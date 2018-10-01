ae379fd807fcc19e178576abe8f3146dceac51c4
Charles-Auguste Marois <charles.marois@polymtl.ca>
2018-10-01 13:54:24 -0400

    Disable routing events in console

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -64,8 +64,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
+      appRoutes // <-- debugging purposes only
     ),
     BrowserModule,
     ReactiveFormsModule,
