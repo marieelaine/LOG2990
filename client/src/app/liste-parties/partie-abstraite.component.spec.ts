efd58142bb140313c4ff0f3fadb15543473086c9
marieelaine <marieelaine@github.com>
2018-10-04 14:52:20 -0400

    removed unecessary modules in abstract class testing file

--- client/src/app/liste-parties/partie-abstraite.component.spec.ts
+++ client/src/app/liste-parties/partie-abstraite.component.spec.ts
@@ -1,8 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PartieAbstraiteClass } from './partie-abstraite';
-import { RouterTestingModule } from '@angular/router/testing';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 class AbstractClassInstance extends PartieAbstraiteClass {
 
