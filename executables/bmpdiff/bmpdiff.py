daa066e7e5fd29efa4555c369aae201b6dc9d70d
JeanSeb Boulianne <jeanseb.b@hotmail.com>
2018-10-05 12:32:15 -0400

    Fin executable différences

--- executables/bmpdiff/bmpdiff.py
+++ executables/bmpdiff/bmpdiff.py
@@ -1,12 +1,13 @@
 import PIL
 from PIL import ImageChops
 from PIL import Image
-import numpy as np
 import sys
 import argparse
 import os.path
+import cv2
+import numpy as np
 
-newImage = Image.new("RGB", (640, 480), 0)
+newImage = PIL.ImageChops.invert(Image.new("RGB", (640, 480), 0))
 newpixels = newImage.load()
 
 def file_choices(choices,fname):
@@ -17,48 +18,76 @@ def file_choices(choices,fname):
 
 def compare_images(settings, enlargePixels):
 
+    #Ouverture des images et déclaration de constantes
     image_one = Image.open(settings.imageOriginale)
     image_two = Image.open(settings.imageModifie)
 
-    width = image_one.size[0]
-    height = image_one.size[1]
+    width1 = image_one.size[0]
+    height1 = image_one.size[1]
+    width2 = image_two.size[0]
+    height2 = image_two.size[1]
+
     pixels_one = np.asarray(image_one)
     pixels_two = np.asarray(image_two)
 
-    for y in range(0,width):
-        for x in range(0,height):
+    #Détection de la mauvaise taille d'image
+    if(not (width1 or width2) == 640 or not (height1 or height2) == 480):
+        print("Images de mauvaise taille, veuillez choisir des images de type .bmp de taille 640x480")
+        return
+
+    #Parcours dans l'image pour trouver les pixels différents
+    for y in range(0,width1):
+        for x in range(0,height1):
 
             px_one = pixels_one[x,y]
             px_two = pixels_two[x,y]
+
             if not (px_one==px_two).all():
                 if(enlargePixels):
-                    enlarge_pixels(x,y)
+                    enlarge_pixels(x, y)
                 else:
                     newpixels[y, x] = (0, 0, 0)
-            elif(newpixels[y, x] == (0, 0, 0)):
-                newpixels[y, x] = (255, 255, 255)
 
-    newImage.save(settings.imageSortie)
+    #Sauvegarde de l'image
+    if (check_number_differences(newImage)):
+        newImage.save(settings.imageSortie)
+        print('Image de différence générée avec succès!')
+    else:
+        print("Le nombre de différences entre les deux images ne correspond pas au nombre demandé! \n"
+              "Il faut exactement 7 différences entre les deux images.")
+
+def check_number_differences(newImage):
+
+    #Contouring pour compter les différences
+    lower = np.array([0, 0, 0])
+    upper = np.array([15, 15, 15])
+    image = np.asarray(newImage)
+    shapeMask = cv2.inRange(image, lower, upper)
+
+    (_, cnts, _) = cv2.findContours(shapeMask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    return len(cnts) == 7
 
 def enlarge_pixels(x, y):
+    #Élargissement des différences
     for i in range(-3, 4):
         for j in range(-3, 4):
             if(not ((abs(i) > 1 and abs(j) == 3) or (abs(j) > 1 and abs(i) == 3))):
                 newpixels[y+i,x+j] = (0, 0, 0)
-            else:
-                newpixels[y+i,x+j] = (255, 0, 0)
 
 if __name__ == '__main__':
 
+    #Déclaration des arguments
     parser = argparse.ArgumentParser(
         description="Générer une image en noir et blanc décrivant les différences entre deux images")
     parser.add_argument("-partiel", action="store_true", help="Générer une image de différences partielles")
-    parser.add_argument('imageOriginale', type=lambda s: file_choices("bmp", s))
-    parser.add_argument('imageModifie', type=lambda s: file_choices("bmp", s))
-    parser.add_argument('imageSortie', type=lambda s: file_choices("bmp", s))
+    parser.add_argument('imageOriginale', type=lambda s: file_choices("bmp", s), help="Chemin pointant sur l'image d'origine")
+    parser.add_argument('imageModifie', type=lambda s: file_choices("bmp", s), help="Chemin pointant sur l'image modifiée")
+    parser.add_argument('imageSortie', type=lambda s: file_choices("bmp", s), help="Chemin où l'image de sortie doit être générée")
 
     settings = parser.parse_args()
 
+    #Vérification argument -partiel
     if settings.partiel:
         compare_images(settings, 0)
     else:
