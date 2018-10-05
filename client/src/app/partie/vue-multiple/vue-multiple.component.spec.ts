cb6684cd600f50c28d16ac5513cde210830c9360
Charles-Auguste Marois <charles.marois@polymtl.ca>
2018-10-05 11:56:11 -0400

    Ajout component vue-multiple et fichiers three.js

---
+++ client/src/app/partie/vue-multiple/vue-multiple.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VueMultipleComponent } from './vue-multiple.component';
+
+describe('VueMultipleComponent', () => {
+  let component: VueMultipleComponent;
+  let fixture: ComponentFixture<VueMultipleComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VueMultipleComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VueMultipleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
