320256f62e12b2301016f593d19ddf71cc834f22
Olivier Naud-Dulude <olivier.naud-dulude@hotmail.com>
2018-10-05 15:31:05 -0400

    Enlever les commentaires // Tested

--- client/src/app/admin/dialog-simple/dialog-simple.component.ts
+++ client/src/app/admin/dialog-simple/dialog-simple.component.ts
@@ -40,7 +40,6 @@ export class DialogSimpleComponent {
     private imageService: ImageService) {
     }
 
-  // Tested
   public onFileSelectedImage(event, i) {
     this.currentImageNumber = i;
     const file = event.target.files[0] as File;
@@ -85,14 +84,12 @@ export class DialogSimpleComponent {
     };
   }
 
-  // Tested
   public setWrongImageSizeOrTypeMessage(imageInfo): void {
     this.checkIfWrongImageSize(imageInfo) || this.checkIfWrongImageType() ?
       this.wrongImageSizeOrTypeMessage = "*L'image doit être de format BMP 24 bits et de taille 640 x 480 pixels" :
       this.wrongImageSizeOrTypeMessage = "";
   }
 
-  // Tested
   private checkIfWrongImageSize(imageInfo): Boolean {
     if (imageInfo["size"] !== 24 || imageInfo["width"] !== 640 || imageInfo["height"] !== 480) {
       return true;
@@ -101,7 +98,6 @@ export class DialogSimpleComponent {
     return false;
   }
 
-  // Tested
   private checkIfWrongImageType(): Boolean {
     var isWrongType: Boolean = false;
     this.selectedFiles.forEach((file) => {
@@ -113,19 +109,16 @@ export class DialogSimpleComponent {
     return isWrongType;
   }
 
-  // Tested
   public onNoClick(): void {
     this.dialogRef.close();
   }
 
-  // Tested
   public onAddSimpleGameClick(): void {
     this.setWrongNumberOfImagesMessage();
     this.setOutOfBoundNameLengthMessage();
     this.closeDialogIfRequirements();
   }
 
-  // Tested
   private checkIfWrongNumberOfImages(): Boolean {
     if (this.selectedFiles[0] === undefined || this.selectedFiles[0] === null
       || this.selectedFiles[1] === undefined || this.selectedFiles[1] === null) {
@@ -135,14 +128,12 @@ export class DialogSimpleComponent {
     return false;
   }
 
-  // Tested
   public setWrongNumberOfImagesMessage(): void {
     this.checkIfWrongNumberOfImages() ?
     this.wrongNumberOfImagesMessage = '*Vous devez entrer deux images.' :
     this.wrongNumberOfImagesMessage = "";
   }
 
-  // Tested
   private checkIfOutOfBoundNameLength(): Boolean {
     if (this.data.simpleGameName === "" || this.data.simpleGameName === undefined
     || this.data.simpleGameName.length < 3 || this.data.simpleGameName.length > 20) {
@@ -152,14 +143,12 @@ export class DialogSimpleComponent {
     return false;
   }
 
-  // Tested
   public setOutOfBoundNameLengthMessage(): void {
     this.checkIfOutOfBoundNameLength() ?
       this.outOfBoundNameLengthMessage = "*Le nom du jeu doit être entre 3 et 20 charactères." :
       this.outOfBoundNameLengthMessage = "" ;
   }
 
-  // Tested
   public checkIfNoErrorMessage(): Boolean {
     if (this.outOfBoundNameLengthMessage === ""
     && this.wrongNumberOfImagesMessage === ""
@@ -170,7 +159,6 @@ export class DialogSimpleComponent {
     return false;
   }
 
-  // Tested
   public closeDialogIfRequirements(): void {
    if (this.checkIfNoErrorMessage()) {
       this.onSubmit();
