eff4c69eb2e4b1937bcd6f1eb932b8c93972ceb9
marieelaine <marieelaine@github.com>
2018-10-21 17:08:49 -0400

    clean up du code

--- client/src/app/common/header/header.component.ts
+++ client/src/app/common/header/header.component.ts
@@ -1,47 +1,29 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 import { CookieService } from "ngx-cookie-service";
 import { UserService } from "../../vue-initiale/user.service";
-// import { Subscription } from "rxjs"; // Pour le prochain sprint.
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.css"]
 })
-export class HeaderComponent implements OnInit, OnDestroy {
-  // private subscriptionUsername: Subscription;  // Sera utilise dans le prochain sprint.
-  private usernameCourant: string;
+export class HeaderComponent {
 
   public constructor(
       private router: Router,
       private cookieService: CookieService,
       private userService: UserService) {
-    this.usernameCourant = "";
   }
 
-  public ngOnInit() {
-    // Pour le prochain sprint.
-    // this.subscriptionUsername = this.userService.recevoirNouveauUser().subscribe((username: string) => {
-    //   this.usernameCourant = username;
-    // });
-  }
-
-  ngOnDestroy(): void {
-    // Pour le prochain sprint.
-    // if (this.subscriptionUsername) {
-    //   this.subscriptionUsername.unsubscribe();
-    //  }
-  }
-
-  public OnHeaderTitleClick() {
-    this.router.navigateByUrl("/liste-parties");
-  }
-
-  public onLogout(): void {
+  protected onLogout(): void {
     const cookieUsername: string = this.cookieService.get("username");
     this.cookieService.deleteAll();
     this.userService.delete(cookieUsername);
     this.router.navigate(["/"]);
+  }
+
+  protected OnHeaderTitleClick() {
+    this.router.navigateByUrl("/liste-parties");
   }
 }
--- client/src/app/vue-initiale/user.service.ts
+++ client/src/app/vue-initiale/user.service.ts
@@ -9,30 +9,16 @@ export class UserService {
     private readonly AJOUTER_URL: string = this.BASE_URL + "ajouter";
     private readonly SUPPRIMER_URL: string = this.BASE_URL + "delete/";
 
-    // Pour le prochain sprint.
-    // private usernameCourantSujet: BehaviorSubject<string> = new BehaviorSubject<string>("");
-    // private usernameCourantObservable$: Observable<string> = this.usernameCourantSujet.asObservable();
+    public constructor(private _http: HttpClient) { }
 
-    constructor(private _http: HttpClient) { }
-
-    public register(user: User): Observable<Object> {
+    private register(user: User): Observable<Object> {
         return this._http.post(this.AJOUTER_URL, user, {
             observe: "body",
             headers: new HttpHeaders().append("Content-Type", "application/json")
         });
     }
 
-    public delete(username: string): Promise<Object> {
+    private delete(username: string): Promise<Object> {
         return this._http.delete(this.SUPPRIMER_URL + username).toPromise();
     }
-
-    // Pour le prochain sprint.
-    // public envoieNouveauUser(nouveauUsername: string): void {
-        // this.usernameCourantSujet.next(nouveauUsername);
-    // }
-
-    // Pour le prochain sprint.
-    // public recevoirNouveauUser(): Observable<string> {
-    //     return this.usernameCourantObservable$;
-    // }
 }
