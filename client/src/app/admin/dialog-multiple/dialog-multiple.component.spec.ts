ecdeb86be1a4aabcf7850b12a6616d1828b00ad3
marieelaine <marie-elaine.berube@outlook.com>
2018-10-24 17:45:37 -0400

    Un test renommé.

--- client/src/app/admin/dialog-multiple/dialog-multiple.component.spec.ts
+++ client/src/app/admin/dialog-multiple/dialog-multiple.component.spec.ts
@@ -1,34 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DialogMultipleComponent } from './dialog-multiple.component';
-import { MatDividerModule, MatFormFieldModule, MatCardModule, MatDialogModule, MatDialogRef,
-         MAT_DIALOG_DATA, MatFormFieldControl, MatInputModule } from '@angular/material';
+import {
+    MatDividerModule, MatFormFieldModule, MatCardModule, MatDialogModule, MatDialogRef,
+    MAT_DIALOG_DATA, MatFormFieldControl, MatInputModule
+} from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('DialogMultipleComponent', () => {
-  let component: DialogMultipleComponent;
-  let fixture: ComponentFixture<DialogMultipleComponent>;
+    let component: DialogMultipleComponent;
+    let fixture: ComponentFixture<DialogMultipleComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [DialogMultipleComponent],
-      imports: [MatDividerModule, MatFormFieldModule, MatCardModule, FormsModule,
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [DialogMultipleComponent],
+            imports: [MatDividerModule, MatFormFieldModule, MatCardModule, FormsModule,
                 MatDialogModule, MatInputModule, BrowserAnimationsModule],
-      providers: [
-        { provide: MatDialogRef, useValue: {} },
-        { provide: MAT_DIALOG_DATA, useValue: [] }
-      ]
-    })
-      .compileComponents();
-  }));
+            providers: [
+                { provide: MatDialogRef, useValue: {} },
+                { provide: MAT_DIALOG_DATA, useValue: [] }
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DialogMultipleComponent);
-    component = fixture.componentInstance;
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DialogMultipleComponent);
+        component = fixture.componentInstance;
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('Composant devrait être créé', () => {
+        expect(component).toBeTruthy();
+    });
+
 });
