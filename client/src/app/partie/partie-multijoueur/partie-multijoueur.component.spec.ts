0c2e69eafa89503d8843c17f986f5561e5cd5070
marieelaine <marie-elaine.berube@outlook.com>
2018-10-24 18:17:00 -0400

    Ajustement du service Partie Multiple et composant Partie Multiple.

--- client/src/app/partie/partie-multijoueur/partie-multijoueur.component.spec.ts
+++ client/src/app/partie/partie-multijoueur/partie-multijoueur.component.spec.ts
@@ -1,22 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PartieMultijoueurComponent } from './partie-multijoueur.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('PartieMultijoueurComponent', () => {
   let component: PartieMultijoueurComponent;
   let fixture: ComponentFixture<PartieMultijoueurComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PartieMultijoueurComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PartieMultijoueurComponent ],
+      imports: [HttpClientTestingModule],
+    });
+
     fixture = TestBed.createComponent(PartieMultijoueurComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
