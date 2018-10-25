71243ea632d55d5cccb0baafa77fc9b75cb8303b
marieelaine <marie-elaine.berube@outlook.com>
2018-10-25 12:27:13 -0400

    changement des routes de partie service

--- client/src/app/liste-parties/liste-partie-service.service.ts
+++ client/src/app/liste-parties/liste-partie-service.service.ts
@@ -9,12 +9,14 @@ import { PartieMultiple } from '../admin/dialog-multiple/partie-mutiple';
 })
 export class ListePartieServiceService {
   private readonly BASE_URL: string = "http://localhost:3000/";
-  private readonly GET_LISTE_SIMPLE_URL: string = this.BASE_URL + "partieSimple/getListePartieSimple";
-  private readonly GET_LISTE_MULTIPLE_URL: string = this.BASE_URL + "partieMultiple/getListePartieMultiple";
-  private readonly DELETE_PARTIE_SIMPLE_URL: string = this.BASE_URL + "partieSimple/delete/";
-  private readonly DELETE_PARTIE_MULTIPLE_URL: string = this.BASE_URL + "partieMultiple/delete/";
-  private readonly REINITIALISER_TEMPS_SIMPLE_URL: string = this.BASE_URL + "partieSimple/reinitialiseTemps/";
-  private readonly REINITIALISER_TEMPS_MULTIPLE_URL: string = this.BASE_URL + "partieMultiple/reinitialiseTemps/";
+  private readonly SIMPLE: string = "partieSimple/";
+  private readonly MULTIPLE: string = "partieMultiple/";
+  private readonly GET_LISTE_SIMPLE_URL: string = this.BASE_URL + this.SIMPLE + "getListePartieSimple";
+  private readonly GET_LISTE_MULTIPLE_URL: string = this.BASE_URL + this.MULTIPLE + "getListePartieMultiple";
+  private readonly DELETE_PARTIE_SIMPLE_URL: string = this.BASE_URL + this.SIMPLE + "delete/";
+  private readonly DELETE_PARTIE_MULTIPLE_URL: string = this.BASE_URL + this.MULTIPLE + "delete/";
+  private readonly REINITIALISER_TEMPS_SIMPLE_URL: string = this.BASE_URL + this.SIMPLE + "reinitialiseTemps/";
+  private readonly REINITIALISER_TEMPS_MULTIPLE_URL: string = this.BASE_URL + this.MULTIPLE + "reinitialiseTemps/";
 
   constructor( private http: HttpClient ) {}
 
