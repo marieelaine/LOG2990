f21a71bd905d1bc35d920126b51a0fa27369ca0b
marieelaine <marie-elaine.berube@outlook.com>
2018-10-24 21:39:21 -0400

    Test pour la classe Liste Partie Service.

--- client/src/app/liste-parties/liste-partie-service.service.spec.ts
+++ client/src/app/liste-parties/liste-partie-service.service.spec.ts
@@ -1,23 +1,60 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, getTestBed } from '@angular/core/testing';
 
 import { ListePartieServiceService } from './liste-partie-service.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('ListePartieServiceService', () => {
     let service: ListePartieServiceService;
+    let mockHttp: HttpTestingController;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [
                 ListePartieServiceService,
             ],
-            imports: [HttpClientTestingModule]
+            imports: [
+                HttpClientTestingModule,
+            ]
         });
 
         service = TestBed.get(ListePartieServiceService);
+        mockHttp = TestBed.get(HttpTestingController);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    describe("Fonction getListeImageSimple", () => {
+        it("Devrait faire une requete GET", () => {
+            service.getListeImageSimple().subscribe();
+
+            const req = mockHttp.expectOne("http://localhost:3000/partieSimple/getListePartieSimple");
+            expect(req.request.method).toBe("GET");
+        });
+    });
+
+    describe("Fonction deletePartieSimple", () => {
+        it("Devrait faire une requete DELETE", () => {
+            const id: string = "12345abcde";
+            service.deletePartieSimple(id);
+
+            const req = mockHttp.expectOne("http://localhost:3000/partieSimple/delete/" + id);
+            expect(req.request.method).toBe("DELETE");
+        });
+    });
+
+    describe("Fonction reinitialiserTempsPartie", () => {
+        it("Devrait faire une requete DELETE", () => {
+            const id: string = "12345abcde";
+            service.reinitialiserTempsPartie(id);
+
+            const req = mockHttp.expectOne("http://localhost:3000/partieSimple/reinitialiseTemps/" + id);
+            expect(req.request.method).toBe("GET");
+        });
+    });
+
+    afterEach(() => {
+        mockHttp.verify();
     });
 });
--- client/src/app/vue-initiale/vue-initiale.component.spec.ts
+++ client/src/app/vue-initiale/vue-initiale.component.spec.ts
@@ -1,37 +1,31 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-// import { VueInitialeComponent } from './vue-initiale.component';
-// import { HeaderComponent } from '../common/header/header.component';
-// import { LoginFormComponent } from './login-form/login-form.component';
-// import { MatToolbarModule } from '@angular/material';
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-// import { ParticlesModule } from 'angular-particle';
-// import { CookieService } from 'ngx-cookie-service';
-// import { UserService } from './user.service';
+import { VueInitialeComponent } from "./vue-initiale.component";
+import { HeaderComponent } from "../common/header/header.component";
+import { LoginFormComponent } from "./login-form/login-form.component";
+import { MatToolbarModule } from "@angular/material";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ParticlesModule } from "angular-particle";
+import { CookieService } from "ngx-cookie-service";
+import { UserService } from "./user.service";
 
-// describe('VueInitialeComponent', () => {
-//   let component: VueInitialeComponent;
-//   let fixture: ComponentFixture<VueInitialeComponent>;
+describe("VueInitialeComponent", () => {
+    let component: VueInitialeComponent;
+    let fixture: ComponentFixture<VueInitialeComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ VueInitialeComponent, HeaderComponent, LoginFormComponent ],
-//       imports: [MatToolbarModule, FormsModule, ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule, ParticlesModule],
-//       providers: [CookieService, UserService]
-//     })
-//     .compileComponents();
-//   }));
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [VueInitialeComponent, HeaderComponent, LoginFormComponent],
+            imports: [MatToolbarModule, FormsModule, ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule, ParticlesModule],
+            providers: [CookieService, UserService]
+        });
+        fixture = TestBed.createComponent(VueInitialeComponent);
+        component = fixture.componentInstance;
+    });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(VueInitialeComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+});
