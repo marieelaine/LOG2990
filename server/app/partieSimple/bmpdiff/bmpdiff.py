9429dca0c55feb9c18a47712c0c6f5adcec7cc27
Charles-Auguste Marois <charles.marois.quebec@gmail.com>
2018-10-25 11:08:52 -0400

    methode iterative pour bmpdiff

--- server/app/partieSimple/bmpdiff/bmpdiff.py
+++ server/app/partieSimple/bmpdiff/bmpdiff.py
@@ -6,9 +6,13 @@ import argparse
 import os.path
 import cv2
 import numpy as np
+from scipy.ndimage.measurements import label
 
 newImage = PIL.ImageChops.invert(Image.new("RGB", (640, 480), 0))
 newpixels = newImage.load()
+stack = []
+visited = np.empty(shape=(640,480))
+visited.fill(False)
 
 def file_choices(choices,fname):
     ext = os.path.splitext(fname)[1][1:]
@@ -50,6 +54,7 @@ def compare_images(settings, enlargePixels):
 
     #Sauvegarde de l'image
     if (check_number_differences(newImage)):
+        findPixelsOfDifferences(newpixels)
         newImage.save(settings.imageSortie)
         print('Image de différence générée avec succès!', file=sys.stdout)
         sys.exit(0)
@@ -59,7 +64,7 @@ def compare_images(settings, enlargePixels):
         sys.exit(1)
         
 def check_number_differences(newImage):
-    f = open(str(settings.imageSortie)+".txt",'w+')
+    #f = open(str(settings.imageSortie)+".txt",'w+')
     #Contouring pour compter les différences
     lower = np.array([0, 0, 0])
     upper = np.array([15, 15, 15])
@@ -68,13 +73,13 @@ def check_number_differences(newImage):
     
     (_, cnts, _) = cv2.findContours(shapeMask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
-    compteur = 1
-    for i in cnts:
-        f.write('diff'+str(compteur)+"\n")
-        for j in i:
-            f.write(str(j[0])+'\n')
-        compteur+=1
-    f.write("end\n")
+    #compteur = 1
+    # for i in cnts:
+    #     f.write('diff'+str(compteur)+"\n")dtype=bool
+    #     for j in i:
+    #         f.write(str(j[0])+'\n')
+    #     compteur+=1
+        
     return len(cnts) == 7
 
 def enlarge_pixels(x, y):
@@ -83,6 +88,48 @@ def enlarge_pixels(x, y):
         for j in range(-3, 4):
             if(not ((abs(i) > 1 and abs(j) == 3) or (abs(j) > 1 and abs(i) == 3))):
                 newpixels[y+i,x+j] = (0, 0, 0)
+
+def findPixelsOfDifferences(newpixels):
+    f = open(str(settings.imageSortie)+".txt",'w+')
+    #Parcours dans l'image pour trouver les pixels différents
+    width = 640
+    height = 480
+    compteur = 1
+    
+    for i in range (0, width):
+        for j in range (0, height):
+            px = newpixels[i,j]
+            if(px == (0,0,0) and visited[i,j] == False):
+                f.write("DIFFERENCE\n")
+                visited[i,j] = True
+                stack.append((i,j))
+                checkPixel(f,i,j)
+    f.write("END\n")
+                
+
+def checkPixel(file, x, y):
+    file.write(str([x,y])+'\n')
+    if(newpixels[x+1,y] == (0,0,0) and visited[x+1,y] == False):
+        stack.append((x+1,y))
+        visited[x+1,y] = True
+        checkPixel(file,x+1,y)
+    if(newpixels[x,y+1] == (0,0,0) and visited[x,y+1] == False):
+        stack.append((x,y+1))
+        visited[x,y+1] = True
+        checkPixel(file,x,y+1)
+    if(newpixels[x-1,y] == (0,0,0) and visited[x-1,y] == False):
+        stack.append((x-1,y))
+        visited[x-1,y] = True
+        checkPixel(file,x-1,y)
+    if(newpixels[x,y-1] == (0,0,0) and visited[x,y-1] == False):
+        stack.append((x,y-1))
+        visited[x,y-1] = True
+        checkPixel(file, x,y-1)
+        
+    
+
+
+
 
 if __name__ == '__main__':
 
