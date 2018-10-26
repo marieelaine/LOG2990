50a42179c9d8fc3c6897cb9bf2ca0e7807e8e46c
marieelaine <marie-elaine.berube@outlook.com>
2018-10-25 22:12:26 -0400

    SocketServer empechait les tests de fonctionner. L'implentation sera a revoir.

--- server/app/partieSimple/partie-simple.spec.ts
+++ server/app/partieSimple/partie-simple.spec.ts
@@ -1,23 +1,23 @@
-// import * as assert from "assert";
-// import { DBPartieSimple } from "./partie-simple";
+import * as assert from "assert";
+import { DBPartieSimple } from "./partie-simple";
 
-// describe("Partie Simple BD classe", () => {
-//     let dbPartieSimple: DBPartieSimple;
+describe("Partie Simple BD classe", () => {
+    let dbPartieSimple: DBPartieSimple;
 
-//     beforeEach(() => {
-//         dbPartieSimple = new DBPartieSimple();
-//     });
+    beforeEach(() => {
+        dbPartieSimple = new DBPartieSimple();
+    });
 
-//     describe("Constructeur", () => {
-//         it("Devrait etre defini", () => {
-//             assert(dbPartieSimple);
-//         });
-//         it("Devrait definir tous les attributs", () => {
-//             assert(dbPartieSimple["baseDeDonnees"]);
-//             assert(dbPartieSimple["schemaArray"]);
-//             assert(dbPartieSimple["modelPartieArray"]);
-//             assert(dbPartieSimple["schemaBuffer"]);
-//             assert(dbPartieSimple["modelPartieBuffer"]);
-//         });
-//     });
-// });
+    describe("Constructeur", () => {
+        it("Devrait etre defini", () => {
+            assert(dbPartieSimple);
+        });
+        it("Devrait definir tous les attributs", () => {
+            assert(dbPartieSimple["baseDeDonnees"]);
+            assert(dbPartieSimple["schemaArray"]);
+            assert(dbPartieSimple["modelPartieArray"]);
+            assert(dbPartieSimple["schemaBuffer"]);
+            assert(dbPartieSimple["modelPartieBuffer"]);
+        });
+    });
+});
