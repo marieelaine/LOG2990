0055beafd8ba62d7bbe15a60734a2d6affb67060
marieelaine <marie-elaine.berube@outlook.com>
2018-10-25 20:37:24 -0400

    Tests pour la classe BaseDeDonnees.

--- server/app/baseDeDonnees/baseDeDonnees.spec.ts
+++ server/app/baseDeDonnees/baseDeDonnees.spec.ts
@@ -1,20 +1,31 @@
 
 import { assert } from "chai";
 import { BaseDeDonnees } from "./baseDeDonnees";
+import * as sinon from "sinon";
 
-describe("BaseDeDonnees", () => {
+describe("BaseDeDonnees classe", () => {
     let db: BaseDeDonnees;
+    let connectStub: sinon.SinonStub;
     beforeEach(() => {
         db = new BaseDeDonnees();
+        connectStub = sinon.stub(db.mongoose, "connect").withArgs(sinon.match.string, sinon.match.object);
     });
 
     describe("Constructeur", () => {
         it("Devrait etre defini", () => {
             assert.isDefined(db);
         });
+    });
 
-        it("Devrait avoir un schema", () => {
-            assert.isDefined(db["schema"]);
+    describe("Fonction assurerConnection", () => {
+        it("Devrait appeller la fonction connect de Mongoose", () => {
+            db.assurerConnection();
+
+            assert(connectStub.calledOnce);
         });
+    });
+
+    afterEach(() => {
+        sinon.restore();
     });
 });
