5a85becfd3a34ff3d146ffa5ef14d7fb45ee26ad
marieelaine <marie-elaine.berube@outlook.com>
2018-10-25 20:17:28 -0400

    Ajustements pour que les tests fonctionnents.

--- client/src/app/partie/partie-abstraite-class.spec.ts
+++ client/src/app/partie/partie-abstraite-class.spec.ts
@@ -3,7 +3,9 @@ import { PartieAbstraiteClass } from "./partie-abstraite-class";
 import { tick, fakeAsync } from '@angular/core/testing';
 
 class AbstractClassInstance extends PartieAbstraiteClass {
-
+    protected ajouterTemps(temps: number): boolean {
+        return true;
+    }
 }
 
 describe('PartieAbstraiteComponent', () => {
