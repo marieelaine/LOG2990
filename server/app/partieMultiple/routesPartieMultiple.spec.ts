066f1e157b2f75b37fa315e2d662b6ba481647ea
marieelaine <marie-elaine.berube@outlook.com>
2018-10-25 23:07:20 -0400

    Tests de routes.

--- server/app/partieMultiple/routesPartieMultiple.spec.ts
+++ server/app/partieMultiple/routesPartieMultiple.spec.ts
@@ -1,4 +1,4 @@
-// import { PartieMultipleInterface } from "./partie-multiple";
+import { PartieMultipleInterface } from "./partie-multiple";
 import * as WebRequest from "web-request";
 import * as assert from "assert";
 
@@ -6,10 +6,16 @@ const URL_BASE: string = "http://localhost:3000/partieMultiple/getListePartieMul
 
 describe("Route Partie Multiple classe", () => {
     describe("requete get", () => {
-        it("Devrait retourner des donnees", async () => {
-            await WebRequest.get(URL_BASE);
+        it("Devrait retourner un tableau d'une longueur differente de 0", async () => {
+            const resultat: PartieMultipleInterface[] = await WebRequest.json<PartieMultipleInterface[]>(URL_BASE);
 
-            assert(true);
+            assert.notEqual(resultat.length, 0);
+        });
+
+        it("Devrait retourner des donnees", async () => {
+            const resultat: PartieMultipleInterface[] = await WebRequest.json<PartieMultipleInterface[]>(URL_BASE);
+
+            assert.ok(resultat);
         });
     });
 });
