0c25af3b172e6d21dcf1cb5ab0754e044814f222
marieelaine <marie-elaine.berube@outlook.com>
2018-11-09 12:15:26 -0500

    ajout de constantes pour les routes

--- client/src/app/constantes.ts
+++ client/src/app/constantes.ts
@@ -6,14 +6,14 @@ export const GET_LISTE_SIMPLE_URL: string = BASE_URL + SIMPLE + "getListePartieS
 export const DELETE_PARTIE_SIMPLE_URL: string = BASE_URL + SIMPLE + "delete/";
 export const REINITIALISER_TEMPS_SIMPLE_URL: string = BASE_URL + SIMPLE + "reinitialiseTemps/";
 
-export const GET_PARTIE_SIMPLE_ATTENTE: string = BASE_URL + "getPartieSimpleEnAttente";
-export const ADD_PARTIE_SIMPLE_ATTENTE: string = BASE_URL + "addPartieSimpleEnAttente";
-export const DELETE_PARTIE_SIMPLE_ATTENTE: string = BASE_URL + "deletePartieSimpleEnAttente";
+export const GET_PARTIE_SIMPLE_ATTENTE: string = BASE_URL + "getPartieSimpleEnAttente/";
+export const ADD_PARTIE_SIMPLE_ATTENTE: string = BASE_URL + "addPartieSimpleEnAttente/";
+export const DELETE_PARTIE_SIMPLE_ATTENTE: string = BASE_URL + "deletePartieSimpleEnAttente/";
 
 export const GET_LISTE_MULTIPLE_URL: string = BASE_URL + MULTIPLE + "getListePartieMultiple";
 export const DELETE_PARTIE_MULTIPLE_URL: string = BASE_URL + MULTIPLE + "delete/";
 export const REINITIALISER_TEMPS_MULTIPLE_URL: string = BASE_URL + MULTIPLE + "reinitialiseTemps/";
 
-export const GET_PARTIE_MULTIPLE_ATTENTE: string = BASE_URL + "getPartieSimpleEnAttente";
-export const ADD_PARTIE_MULTIPLE_ATTENTE: string = BASE_URL + "addPartieSimpleEnAttente";
-export const DELETE_PARTIE_MULTIPLE_ATTENTE: string = BASE_URL + "deletePartieSimpleEnAttente";
+export const GET_PARTIE_MULTIPLE_ATTENTE: string = BASE_URL + "getPartieSimpleEnAttente/";
+export const ADD_PARTIE_MULTIPLE_ATTENTE: string = BASE_URL + "addPartieSimpleEnAttente/";
+export const DELETE_PARTIE_MULTIPLE_ATTENTE: string = BASE_URL + "deletePartieSimpleEnAttente/";
