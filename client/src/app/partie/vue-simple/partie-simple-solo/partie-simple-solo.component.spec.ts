f3f5a1dae9ee6d65de3786be80709e72b6f81736
Marie-Elaine Berube <marie-elaine.berube@outlook.com>
2018-11-10 22:52:20 -0500

    Ajustement des providers dans les tests. Maintenant tous fonctionnels.

--- client/src/app/partie/vue-simple/partie-simple-solo/partie-simple-solo.component.spec.ts
+++ client/src/app/partie/vue-simple/partie-simple-solo/partie-simple-solo.component.spec.ts
@@ -1,17 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { PartieSimpleSoloComponent } from './partie-simple-solo.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { MatCardModule } from '@angular/material';
-import { ErrorHandler } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ActivatedRouteMock } from 'src/testing/mocks';
-import { ChatComponent } from 'src/app/chat/chat.component';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { PartieSimpleSoloComponent } from "./partie-simple-solo.component";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { MatCardModule } from "@angular/material";
+import { ActivatedRoute } from "@angular/router";
+import { ActivatedRouteMock } from "src/testing/mocks";
+import { ChatComponent } from "src/app/chat/chat.component";
+import { CookieService } from "ngx-cookie-service";
 
-describe('PartieSoloComponent', () => {
+describe("PartieSoloComponent", () => {
+    let mockCookieService: jasmine.SpyObj<CookieService>;
+
     let component: PartieSimpleSoloComponent;
     let fixture: ComponentFixture<PartieSimpleSoloComponent>;
 
-    beforeEach(async(() => {
+    beforeEach(() => {
+        mockCookieService = jasmine.createSpyObj([""]);
+
         TestBed.configureTestingModule({
             declarations: [PartieSimpleSoloComponent, ChatComponent],
             imports: [
@@ -19,23 +23,16 @@ describe('PartieSoloComponent', () => {
                 MatCardModule
             ],
             providers: [
-                {
-                    provide: ActivatedRoute,
-                    useClass: ActivatedRouteMock
-                },
+                { provide: ActivatedRoute, useClass: ActivatedRouteMock },
+                { provide: CookieService, useValue: mockCookieService },
             ]
-        })
-            .compileComponents()
-            .catch(() => ErrorHandler);
-    }));
+        });
 
-    beforeEach(() => {
         fixture = TestBed.createComponent(PartieSimpleSoloComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
     });
 
-    it('should create', () => {
+    it("should create", () => {
         expect(component).toBeTruthy();
     });
 });
