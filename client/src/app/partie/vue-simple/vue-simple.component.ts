e0421a5b59632a8b83ec3adf604192345b98c9f0
Bernard Meunier <bern97@hotmail.ca>
2018-11-13 15:41:16 -0500

    effet pou rles erreurs sond et cursor

--- client/src/app/partie/vue-simple/vue-simple.component.ts
+++ client/src/app/partie/vue-simple/vue-simple.component.ts
@@ -1,9 +1,8 @@
-import { Component, ErrorHandler } from '@angular/core';
+import { Component } from '@angular/core';
 import { PartieAbstraiteClass } from '../partie-abstraite-class';
 import { PartieSimple } from 'src/app/admin/dialog-simple/partie-simple';
 import { ActivatedRoute } from '@angular/router';
 import { PartieService } from '../partie.service';
-import { TempsUser } from 'src/app/admin/dialog-abstrait';
 import { CookieService } from 'ngx-cookie-service';
 
 @Component({
@@ -36,44 +35,47 @@ export class VueSimpleComponent extends PartieAbstraiteClass {
   }
 
   protected testerPourDiff(offsetX, offsetY): void {
-      if (this.partieCommence) {
+      if (this.partieCommence && !this.penaliteEtat) {
 
           const coords = offsetX + "," + offsetY;
           let i: number = 0;
           for (const diff of this.partie["_imageDiff"]) {
               for (const pixel of diff) {
                   if (coords === pixel) {
-                      this.differenceTrouver(i);
+                      if (!this.diffTrouvee.includes(i)) {
+                          this.differenceTrouver(i);
+
+                          return;
+                      }
                   }
               }
               i++;
           }
+          this.penalite(offsetX, offsetY);
       }
   }
 
   protected differenceTrouver(i): void {
-      if (!this.diffTrouvee.includes(i)) {
-          this.diffTrouvee.push(i);
-          this.trouverDifference();
+      this.diffTrouvee.push(i);
+      this.trouverDifference();
 
-          const contextG = this.canvas.toArray()[0].nativeElement.getContext("2d");
-          const imageDataG = contextG.getImageData(0, 0, 640, 480);
-          const dataG = imageDataG.data;
+      const contextG = this.canvas.toArray()[0].nativeElement.getContext("2d");
+      const imageDataG = contextG.getImageData(0, 0, 640, 480);
+      const dataG = imageDataG.data;
 
-          const contextD = this.canvas.toArray()[1].nativeElement.getContext("2d");
-          const imageDataD = contextD.getImageData(0, 0, 640, 480);
-          const dataD = imageDataD.data;
+      const contextD = this.canvas.toArray()[1].nativeElement.getContext("2d");
+      const imageDataD = contextD.getImageData(0, 0, 640, 480);
+      const dataD = imageDataD.data;
 
-          for (const pixel of this.partie["_imageDiff"][i]) {
-              const x: number = Number(pixel.split(",")[0]);
-              const y: number = Number(pixel.split(",")[1]);
-              const dim = (y * 640 * 4) + (x * 4);
+      for (const pixel of this.partie["_imageDiff"][i]) {
+          const x: number = Number(pixel.split(",")[0]);
+          const y: number = Number(pixel.split(",")[1]);
+          const dim = (y * 640 * 4) + (x * 4);
 
-              dataD[dim] = dataG[dim];
-              dataD[dim + 1] = dataG[dim + 1];
-              dataD[dim + 2] = dataG[dim + 2];
-          }
-          contextD.putImageData(imageDataD, 0, 0);
+          dataD[dim] = dataG[dim];
+          dataD[dim + 1] = dataG[dim + 1];
+          dataD[dim + 2] = dataG[dim + 2];
       }
+      contextD.putImageData(imageDataD, 0, 0);
   }
 }
