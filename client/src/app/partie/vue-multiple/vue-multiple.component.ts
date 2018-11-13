f1c336bdcb176b26b205b56604fde52bf7c2236b
Bernard Meunier <bern97@hotmail.ca>
2018-11-13 00:11:05 -0500

    Detection diff pour vue multiple

--- client/src/app/partie/vue-multiple/vue-multiple.component.ts
+++ client/src/app/partie/vue-multiple/vue-multiple.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, ErrorHandler} from '@angular/core';
 import { PartieAbstraiteClass } from "../partie-abstraite-class";
 import { ActivatedRoute} from "@angular/router";
 import { PartieMultiple} from "../../admin/dialog-multiple/partie-multiple";
 import { PartieService} from "../partie.service";
+import {TempsUser} from "../../admin/dialog-abstrait";
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-vue-multiple',
@@ -14,8 +16,9 @@ export class VueMultipleComponent extends PartieAbstraiteClass {
     protected partie: PartieMultiple;
 
     public constructor(protected route: ActivatedRoute,
-                       protected partieService: PartieService) {
-        super(route, partieService, false);
+                       protected partieService: PartieService,
+                       protected cookieService: CookieService) {
+        super(route, partieService, cookieService, false);
         this.differenceRestantes = 14;
     }
 
@@ -38,17 +41,15 @@ export class VueMultipleComponent extends PartieAbstraiteClass {
         if (this.partieCommence) {
 
             const coords = "[" + event.offsetX + ", " + event.offsetY + "]";
+            const source: string = event.srcElement.id === "canvasG1" || event.srcElement.id === "canvasD1"
+                ? "_imageDiff1"
+                : "_imageDiff2";
 
             let i: number = 0;
-            for (const diff of this.partie["_imageDiff"]) {
+            for (const diff of this.partie[source]) {
                 for (const pixel of diff) {
-
                     if (coords === pixel) {
-                        if (event.srcElement.id === "canvasD1") {
-                            this.differenceTrouver(i, true);
-                        } else if (event.srcElement.id === "canvasD2") {
-                            this.differenceTrouver(i, false);
-                        }
+                        this.differenceTrouver(i, source);
                     }
                 }
                 i++;
@@ -56,29 +57,29 @@ export class VueMultipleComponent extends PartieAbstraiteClass {
         }
     }
 
-    protected differenceTrouver(i, bool): void {
+    protected differenceTrouver(i: number, src: string): void {
         if (!this.diffTrouvee.includes(i)) {
             this.diffTrouvee.push(i);
             this.trouverDifference();
 
             let contextG: CanvasRenderingContext2D;
             let contextD: CanvasRenderingContext2D;
-
-            (bool) ? contextG = this.canvas.toArray()[0].nativeElement.getContext("2d") : contextG =
-                this.canvas.toArray()[2].nativeElement.getContext("2d");
-            (bool) ? contextD = this.canvas.toArray()[1].nativeElement.getContext("2d") : contextD =
-                this.canvas.toArray()[3].nativeElement.getContext("2d");
-
+            if (src === "_imageDiff1") {
+                contextG = this.canvas.toArray()[0].nativeElement.getContext("2d");
+                contextD = this.canvas.toArray()[1].nativeElement.getContext("2d");
+            } else {
+                contextG = this.canvas.toArray()[2].nativeElement.getContext("2d");
+                contextD = this.canvas.toArray()[3].nativeElement.getContext("2d");
+            }
             const imageDataG = contextG.getImageData(0, 0, 640, 480);
             const dataG = imageDataG.data;
             const imageDataD = contextD.getImageData(0, 0, 640, 480);
             const dataD = imageDataD.data;
 
-            for (const pixel of this.partie["_imageDiff"][i]) {
+            for (const pixel of this.partie[src][i]) {
                 const x: number = Number(pixel.split(",")[0]);
                 const y: number = Number(pixel.split(",")[1]);
                 const dim = (y * 640 * 4) + (x * 4);
-
                 dataD[dim] = dataG[dim];
                 dataD[dim + 1] = dataG[dim + 1];
                 dataD[dim + 2] = dataG[dim + 2];
