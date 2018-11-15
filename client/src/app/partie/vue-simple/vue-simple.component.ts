e810512a303fa78580b92bd3656794727368cc70
marieelaine <marie-elaine.berube@outlook.com>
2018-11-15 18:06:54 -0500

    Fix un typo dans des routes

--- client/src/app/partie/vue-simple/vue-simple.component.ts
+++ client/src/app/partie/vue-simple/vue-simple.component.ts
@@ -41,7 +41,6 @@ export class VueSimpleComponent extends PartieAbstraiteClass {
     }
 
     protected testerPourDiff(event): void {
-        console.log(event.offsetX, event.offsetY);
         if (this.partieCommence && !this.penaliteEtat) {
             const coords = event.offsetX + "," + event.offsetY;
             let i: number = 0;
