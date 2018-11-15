201441d1d8a6b8dfa58cc27fc368be6ab10f34cc
Marie-Elaine Berube <marie-elaine.berube@outlook.com>
2018-11-15 17:23:48 -0500

    Suppression de composant non utilisé

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,19 +10,18 @@ import { HttpClientModule } from "@angular/common/http";
 import { RouterModule, Routes } from "@angular/router";
 import { SocketIoModule, SocketIoConfig } from 'ng6-socket-io';
 import { CookieService } from "ngx-cookie-service";
-
-import { MatToolbarModule,
-        MatCardModule,
-        MatBadgeModule,
-        MatButtonModule,
-        MatDialogModule,
-        MatDividerModule,
-        MatMenuModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatCheckboxModule
-      } from '@angular/material';
-
+import {
+    MatToolbarModule,
+    MatCardModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule
+} from '@angular/material';
 import { ListePartiesComponent } from "./liste-parties/liste-parties.component";
 import { ChronoComponent } from './chrono/chrono.component';
 import { HeaderComponent } from "./common/header/header.component";
@@ -44,89 +43,86 @@ import { SocketClientService } from "./socket/socket-client.service";
 import { ListePartieServiceService } from "./liste-parties/liste-partie-service.service";
 import { DialogErreurComponent } from "./admin/dialog-erreur/dialog-erreur.component";
 import { DialogVueAttenteComponent } from './liste-parties/dialog-vue-attente/dialog-vue-attente.component';
-import { PartieSimpleSoloComponent } from "./partie/vue-simple/partie-simple-solo/partie-simple-solo.component";
 import { ChatComponent } from './chat/chat.component';
 import { VueSimpleComponent } from './partie/vue-simple/vue-simple.component';
-import { PartieMultipleSoloComponent } from './partie/vue-multiple/partie-multiple-solo/partie-multiple-solo.component';
-// tslint:disable-next-line:max-line-length
-import { PartieMultipleMultijoueurComponent } from './partie/vue-multiple/partie-multiple-multijoueur/partie-multiple-multijoueur.component';
+import {
+    PartieMultipleMultijoueurComponent
+} from './partie/vue-multiple/partie-multiple-multijoueur/partie-multiple-multijoueur.component';
 
 const config: SocketIoConfig = { url: 'http://localhost:4200', options: {} };
 
 const appRoutes: Routes = [
-  { path: "", component: VueInitialeComponent },
-  { path: "header", component: HeaderComponent },
-  { path: "admin", component: AdminComponent },
-  { path: "liste-parties", component: ListePartiesComponent },
-  { path: "chrono", component: ChronoComponent },
-  { path: "partie-simple-solo/:idPartie", component: PartieSimpleSoloComponent },
-  { path: "partie-multi/:idPartie", component: PartieSimpleMultijoueurComponent },
-  { path: "partie-multiple/:idPartie", component: VueMultipleComponent },
+    { path: "", component: VueInitialeComponent },
+    { path: "header", component: HeaderComponent },
+    { path: "admin", component: AdminComponent },
+    { path: "liste-parties", component: ListePartiesComponent },
+    { path: "chrono", component: ChronoComponent },
+    { path: "partie-simple-solo/:idPartie", component: VueMultipleComponent },
+    { path: "partie-multi/:idPartie", component: PartieSimpleMultijoueurComponent },
+    { path: "partie-multiple/:idPartie", component: VueMultipleComponent },
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    VueInitialeComponent,
-    AdminComponent,
-    ListePartiesComponent,
-    LoginFormComponent,
-    ChronoComponent,
-    PartieSimpleSoloComponent,
-    ListePartieSimpleComponent,
-    ListePartieMultipleComponent,
-    DialogSimpleComponent,
-    DialogMultipleComponent,
-    PartieSimpleMultijoueurComponent,
-    VueMultipleComponent,
-    DialogConfirmationComponent,
-    DialogErreurComponent,
-    DialogVueAttenteComponent,
-    ChatComponent,
-    VueSimpleComponent,
-    PartieMultipleSoloComponent,
-    PartieMultipleMultijoueurComponent,
-  ],
-  imports: [
-    RouterModule.forRoot(
-      appRoutes // <-- debugging purposes only
-    ),
-    BrowserModule,
-    ReactiveFormsModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    ModalModule,
-    NgbModule,
-    BrowserAnimationsModule,
-    MatDialogModule,
-    MatDividerModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatBadgeModule,
-    MatCheckboxModule,
-    MatMenuModule,
-    MatFormFieldModule,
-    MatInputModule,
-    ParticlesModule,
-    SocketIoModule.forRoot(config),
-  ],
-  providers: [BasicService,
-              CookieService,
-              UserService,
-              PartieSimpleService,
-              PartieMultipleService,
-              SocketClientService,
-              ListePartieServiceService],
-  bootstrap: [AppComponent],
-  entryComponents: [
-    DialogSimpleComponent,
-    DialogMultipleComponent,
-    DialogConfirmationComponent,
-    DialogErreurComponent,
-    DialogVueAttenteComponent
-  ],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        VueInitialeComponent,
+        AdminComponent,
+        ListePartiesComponent,
+        LoginFormComponent,
+        ChronoComponent,
+        ListePartieSimpleComponent,
+        ListePartieMultipleComponent,
+        DialogSimpleComponent,
+        DialogMultipleComponent,
+        PartieSimpleMultijoueurComponent,
+        VueMultipleComponent,
+        DialogConfirmationComponent,
+        DialogErreurComponent,
+        DialogVueAttenteComponent,
+        ChatComponent,
+        VueSimpleComponent,
+        PartieMultipleMultijoueurComponent,
+    ],
+    imports: [
+        RouterModule.forRoot(
+            appRoutes // <-- debugging purposes only
+        ),
+        BrowserModule,
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        ModalModule,
+        NgbModule,
+        BrowserAnimationsModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatCardModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatBadgeModule,
+        MatCheckboxModule,
+        MatMenuModule,
+        MatFormFieldModule,
+        MatInputModule,
+        ParticlesModule,
+        SocketIoModule.forRoot(config),
+    ],
+    providers: [BasicService,
+        CookieService,
+        UserService,
+        PartieSimpleService,
+        PartieMultipleService,
+        SocketClientService,
+        ListePartieServiceService],
+    bootstrap: [AppComponent],
+    entryComponents: [
+        DialogSimpleComponent,
+        DialogMultipleComponent,
+        DialogConfirmationComponent,
+        DialogErreurComponent,
+        DialogVueAttenteComponent
+    ],
 })
 export class AppModule { }
