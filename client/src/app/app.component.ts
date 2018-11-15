7bdebb50e8ff1552fcb0bb47ad2f568a0dccd7fa
marieelaine <marie-elaine.berube@outlook.com>
2018-11-15 17:57:12 -0500

    enlever des trucs inutiles dans app-component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,19 +1,14 @@
 import { Component, OnInit } from "@angular/core";
-import { Message } from "../../../common/communication/message";
-import { BasicService } from "./basic.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit {
-    public constructor(private basicService: BasicService) { }
+export class AppComponent {
+    public constructor() { }
 
     public readonly title: string = "LOG2990";
     public message: string;
 
-    public ngOnInit(): void {
-        this.basicService.basicGet().subscribe((message: Message) => this.message = message.title + message.body);
-    }
 }
