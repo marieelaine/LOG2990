92334414038eeeb66b5177d7b5a28f421f3d8513
Marie-Elaine Berube <marie-elaine.berube@outlook.com>
2018-11-15 17:42:51 -0500

    Single to double quote

--- client/src/app/liste-parties/liste-parties.component.spec.ts
+++ client/src/app/liste-parties/liste-parties.component.spec.ts
@@ -1,16 +1,16 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ListePartiesComponent } from './liste-parties.component';
-import { MatCardModule, MAT_DIALOG_DATA, MatDialogModule, MatMenuTrigger, MatMenuModule } from '@angular/material';
-import { RouterTestingModule } from '@angular/router/testing';
-import { ListePartieSimpleComponent } from './liste-partie-simple/liste-partie-simple.component';
-import { ListePartieMultipleComponent } from './liste-partie-multiple/liste-partie-multiple.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { AdminComponent } from '../admin/admin.component';
-import { TempsUser } from '../admin/dialog-abstrait';
-import { VueSimpleComponent } from '../partie/vue-simple/vue-simple.component';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ListePartiesComponent } from "./liste-parties.component";
+import { MatCardModule, MAT_DIALOG_DATA, MatDialogModule, MatMenuTrigger, MatMenuModule } from "@angular/material";
+import { RouterTestingModule } from "@angular/router/testing";
+import { ListePartieSimpleComponent } from "./liste-partie-simple/liste-partie-simple.component";
+import { ListePartieMultipleComponent } from "./liste-partie-multiple/liste-partie-multiple.component";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { AdminComponent } from "../admin/admin.component";
+import { TempsUser } from "../admin/dialog-abstrait";
+import { VueSimpleComponent } from "../partie/vue-simple/vue-simple.component";
 
-describe('ListePartiesComponent', () => {
+describe("ListePartiesComponent", () => {
     let component: ListePartiesComponent;
     let fixture: ComponentFixture<ListePartiesComponent>;
     const titleTest: string = "NSuccess";
@@ -46,25 +46,25 @@ describe('ListePartiesComponent', () => {
         component = fixture.componentInstance;
     });
 
-    it('should create', () => {
+    it("should create", () => {
         expect(component).toBeTruthy();
     });
 
-    it('should change jouerOuReinitialiser and creerOuSupprimer to "Jouer" and "Supprimer" when url is /liste-parties', () => {
-        component["setjouerOuReinitialiserAndcreerOuSupprimer"]('/liste-parties');
-        expect(component["jouerOuReinitialiser"]).toBe('Jouer');
-        expect(component["creerOuSupprimer"]).toBe('Créer');
+    it("should change jouerOuReinitialiser and creerOuSupprimer to 'Jouer' and 'Supprimer' when url is /liste-parties", () => {
+        component["setjouerOuReinitialiserAndcreerOuSupprimer"]("/liste-parties");
+        expect(component["jouerOuReinitialiser"]).toBe("Jouer");
+        expect(component["creerOuSupprimer"]).toBe("Créer");
         expect(component["isListePartiesMode"]).toBe(true);
     });
 
-    it('should change jouerOuReinitialiser and creerOuSupprimer to "Reinitialiser" and "Supprimer" when url is /admin', () => {
-        component["setjouerOuReinitialiserAndcreerOuSupprimer"]('/admin');
-        expect(component["jouerOuReinitialiser"]).toBe('Réinitialiser');
-        expect(component["creerOuSupprimer"]).toBe('Supprimer');
+    it("should change jouerOuReinitialiser and creerOuSupprimer to 'Reinitialiser' and 'Supprimer' when url is /admin", () => {
+        component["setjouerOuReinitialiserAndcreerOuSupprimer"]("/admin");
+        expect(component["jouerOuReinitialiser"]).toBe("Réinitialiser");
+        expect(component["creerOuSupprimer"]).toBe("Supprimer");
         expect(component["isAdminMode"]).toBe(true);
     });
 
-    it('should return array of sorted times', () => {
+    it("should return array of sorted times", () => {
         const user1: TempsUser = new TempsUser("user1", 2);
         const user2: TempsUser = new TempsUser("user2", 1);
         const sortingTimesTest: TempsUser[] = [user1, user2];
@@ -74,25 +74,25 @@ describe('ListePartiesComponent', () => {
         expect(sortedArray).toEqual(expectedArray);
     });
 
-    it('should return array of sorted times', () => {
+    it("should return array of sorted times", () => {
         const temps = new TempsUser("", 61);
         expect(component["getDisplayTime"](temps)).toBe("1:01");
     });
 
-    it('genererTableauTempsAleatoires devrait creer une tableau de taille 3', () => {
+    it("genererTableauTempsAleatoires devrait creer une tableau de taille 3", () => {
         const array = component["genererTableauTempsAleatoires"]();
         expect(array.length).toEqual(3);
     });
 
-    it('should return title without first letter', () => {
+    it("should return title without first letter", () => {
         expect(component["getTitleWithoutFirstLetter"](titleTest)).toBe("Success");
     });
 
-    it('should return title first letter', () => {
+    it("should return title first letter", () => {
         expect(component["getTitleFirstLetter"](titleTest)).toBe("N");
     });
 
-    it('should change attribute modes', () => {
+    it("should change attribute modes", () => {
         component["setToJouerAndCreer"]();
         expect(component["isAdminMode"]).toBeFalsy();
         expect(component["isListePartiesMode"]).toBeTruthy();
@@ -100,7 +100,7 @@ describe('ListePartiesComponent', () => {
         expect(component["creerOuSupprimer"]).toBe("Créer");
     });
 
-    it('should change attribute modes', () => {
+    it("should change attribute modes", () => {
         component["setToReinitialiserAndSupprimer"]();
         expect(component["isAdminMode"]).toBeTruthy();
         expect(component["isListePartiesMode"]).toBeFalsy();
