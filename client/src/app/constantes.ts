a3a89d6b667e27a98f11a0d5f0191d6b015c9ed6
marieelaine <marie-elaine.berube@outlook.com>
2018-11-15 11:58:31 -0500

    ajout des routes dnas le fichier de constantes

--- client/src/app/constantes.ts
+++ client/src/app/constantes.ts
@@ -17,3 +17,6 @@ export const REINITIALISER_TEMPS_MULTIPLE_URL: string = BASE_URL + MULTIPLE + "r
 export const GET_PARTIE_MULTIPLE_ATTENTE: string = BASE_URL + "getPartieSimpleEnAttente/";
 export const ADD_PARTIE_MULTIPLE_ATTENTE: string = BASE_URL + "addPartieSimpleEnAttente/";
 export const DELETE_PARTIE_MULTIPLE_ATTENTE: string = BASE_URL + "deletePartieSimpleEnAttente/";
+
+export const GETPARTIESIMPLE_URL: string = BASE_URL + SIMPLE + "getPartieSimple/";
+export const GETPARTIEMULTIPLE_URL: string = BASE_URL + MULTIPLE + "getPartieMultiple/";
