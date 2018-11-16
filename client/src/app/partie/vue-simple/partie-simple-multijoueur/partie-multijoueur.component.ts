45c5755c1fa1c7502027d5ac2e08cfca40fd6835
Bernard Meunier <bern97@hotmail.ca>
2018-11-15 19:27:46 -0500

    ' a "

--- client/src/app/partie/vue-simple/partie-simple-multijoueur/partie-multijoueur.component.ts
+++ client/src/app/partie/vue-simple/partie-simple-multijoueur/partie-multijoueur.component.ts
@@ -1,22 +1,23 @@
-import { Component } from '@angular/core';
-import { PartieService} from "../../partie.service";
-import { VueSimpleComponent } from '../vue-simple.component';
-import { ActivatedRoute } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
+import {Component} from "@angular/core";
+import {PartieService} from "../../partie.service";
+import {VueSimpleComponent} from "../vue-simple.component";
+import {ActivatedRoute} from "@angular/router";
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
-    selector: 'app-partie-multijoueur',
-    templateUrl: '../vue-simple.component.html',
-    styleUrls: ['../vue-simple.component.css'],
+    selector: "app-partie-multijoueur",
+    templateUrl: "../vue-simple.component.html",
+    styleUrls: ["../vue-simple.component.css"],
     providers: [PartieService]
 })
 
 export class PartieSimpleMultijoueurComponent extends VueSimpleComponent {
 
-    constructor(protected route: ActivatedRoute,
-                protected partieService: PartieService,
-                protected cookieService: CookieService) {
+    protected constructor(protected route: ActivatedRoute,
+                          protected partieService: PartieService,
+                          protected cookieService: CookieService) {
         super(route, partieService, cookieService);
     }
+
     // TODO partie multijoueur pour le sprint 4
 }
