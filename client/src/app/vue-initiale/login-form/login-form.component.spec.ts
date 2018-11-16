458ed8744b6ef297409e70829ad77b95b7dda392
Charles-Auguste Marois <charles.marois@polymtl.ca>
2018-11-15 20:12:48 -0500

    correction ts lint login form spec

--- client/src/app/vue-initiale/login-form/login-form.component.spec.ts
+++ client/src/app/vue-initiale/login-form/login-form.component.spec.ts
@@ -1,14 +1,14 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { LoginFormComponent } from './login-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { LoginFormComponent } from "./login-form.component";
+import { FormsModule, ReactiveFormsModule, AbstractControl, ValidationErrors } from "@angular/forms";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { RouterTestingModule } from "@angular/router/testing";
-import { ParticlesModule } from 'angular-particle';
-import { CookieService } from 'ngx-cookie-service';
-import { ErrorHandler } from '@angular/core';
+import { ParticlesModule } from "angular-particle";
+import { CookieService } from "ngx-cookie-service";
+import { ErrorHandler } from "@angular/core";
 
-describe('LoginFormComponent', () => {
+describe("LoginFormComponent", () => {
 let component: LoginFormComponent;
 let fixture: ComponentFixture<LoginFormComponent>;
 
@@ -29,49 +29,49 @@ beforeEach(() => {
     fixture.detectChanges();
 });
 
-it('should create', () => {
+it("should create", () => {
     expect(component).toBeTruthy();
 });
 
-it('form invalid when empty', () => {
+it("form invalid when empty", () => {
     expect(component["loginForm"].valid).toBeFalsy();
 });
 
 // tslint:disable-next-line:max-func-body-length
-it('username field validity', () => {
-    let errors = {};
-    const username = component["loginForm"].controls['username'];
+it("username field validity", () => {
+    let errors: ValidationErrors = {};
+    const username: AbstractControl  = component["loginForm"].controls["username"];
     expect(username.valid).toBeFalsy();
 
     // username field is required
     errors = username.errors || {};
-    expect(errors['required']).toBeTruthy();
+    expect(errors["required"]).toBeTruthy();
 
-    // Set username to something that doesn't respect the pattern
+    // Set username to something that doesn"t respect the pattern
     username.setValue("tEst10-;");
     errors = username.errors || {};
-    expect(errors['required']).toBeFalsy();
-    expect(errors['pattern']).toBeTruthy();
+    expect(errors["required"]).toBeFalsy();
+    expect(errors["pattern"]).toBeTruthy();
 
-    // Set username to something that doesn't respect the pattern
+    // Set username to something that doesn"t respect the pattern
     username.setValue("tE");
     errors = username.errors || {};
-    expect(errors['required']).toBeFalsy();
-    expect(errors['minlength']).toBeTruthy();
+    expect(errors["required"]).toBeFalsy();
+    expect(errors["minlength"]).toBeTruthy();
 
-    // Set username to something that doesn't respect the pattern
+    // Set username to something that doesn"t respect the pattern
     username.setValue("tEst10000000000");
     errors = username.errors || {};
-    expect(errors['required']).toBeFalsy();
-    expect(errors['maxlength']).toBeTruthy();
+    expect(errors["required"]).toBeFalsy();
+    expect(errors["maxlength"]).toBeTruthy();
 
     // Set username to something correct
     username.setValue("admiN1");
     errors = username.errors || {};
-    expect(errors['required']).toBeFalsy();
-    expect(errors['minlength']).toBeFalsy();
-    expect(errors['maxlength']).toBeFalsy();
-    expect(errors['pattern']).toBeFalsy();
+    expect(errors["required"]).toBeFalsy();
+    expect(errors["minlength"]).toBeFalsy();
+    expect(errors["maxlength"]).toBeFalsy();
+    expect(errors["pattern"]).toBeFalsy();
 });
 
 });
