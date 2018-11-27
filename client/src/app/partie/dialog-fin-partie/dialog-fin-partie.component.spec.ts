ce6a894fc7951972777cc0f624e0e8e8c350da18
Charles-Auguste Marois <charles.marois@polymtl.ca>
2018-11-27 12:35:43 -0500

    Fix tests matdialog

--- client/src/app/partie/dialog-fin-partie/dialog-fin-partie.component.spec.ts
+++ client/src/app/partie/dialog-fin-partie/dialog-fin-partie.component.spec.ts
@@ -1,14 +1,34 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { ErrorHandler } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { RouterTestingModule } from "@angular/router/testing";
+import { Data } from "@angular/router";
 import { DialogFinPartieComponent } from "./dialog-fin-partie.component";
 
 describe("DialogFinPartieComponent", () => {
   let component: DialogFinPartieComponent;
   let fixture: ComponentFixture<DialogFinPartieComponent>;
+  let mockMatDialogRef: jasmine.SpyObj<MatDialogRef<DialogFinPartieComponent>>;
+  let mockMatDialogData: jasmine.SpyObj<Data>;
 
   beforeEach(async(() => {
+    mockMatDialogRef = jasmine.createSpyObj([
+      "open"
+    ]);
+
+    mockMatDialogData = jasmine.createSpyObj([
+      ""
+    ]);
+
     TestBed.configureTestingModule({
-      declarations: [ DialogFinPartieComponent ]
+      declarations: [ DialogFinPartieComponent ],
+      imports: [
+        RouterTestingModule
+      ],
+      providers : [
+        { provide: MatDialogRef, useValue: mockMatDialogRef },
+        { provide: MAT_DIALOG_DATA, useValue: mockMatDialogData }
+      ]
     })
     .compileComponents().catch(() => ErrorHandler);
   }));
