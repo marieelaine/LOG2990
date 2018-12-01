c53f0b3b5ed97eff56ba89fb19ad3ae7adbab3e4
Bernard Meunier <bern97@hotmail.ca>
2018-12-01 16:55:44 -0500

    correction appmodule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from "@angular/core";
-import { ReactiveFormsModule, FormsModule } from "@angular/forms";
-import { ModalModule } from "ngx-bootstrap";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { AppComponent } from "./app.component";
-import { BasicService } from "./basic.service";
-import { HttpClientModule } from "@angular/common/http";
-import { RouterModule, Routes } from "@angular/router";
-import { SocketIoModule, SocketIoConfig } from "ng6-socket-io";
-import { CookieService } from "ngx-cookie-service";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {NgModule} from "@angular/core";
+import {ReactiveFormsModule, FormsModule} from "@angular/forms";
+import {ModalModule} from "ngx-bootstrap";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {AppComponent} from "./app.component";
+import {BasicService} from "./basic.service";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+import {SocketIoModule, SocketIoConfig} from "ng6-socket-io";
+import {CookieService} from "ngx-cookie-service";
 import {
     MatToolbarModule,
     MatCardModule,
@@ -22,41 +22,41 @@ import {
     MatInputModule,
     MatCheckboxModule
 } from "@angular/material";
-import { ListePartiesComponent } from "./liste-parties/liste-parties.component";
-import { ChronoComponent } from "./chrono/chrono.component";
-import { HeaderComponent } from "./common/header/header.component";
-import { VueInitialeComponent } from "./vue-initiale/vue-initiale.component";
-import { AdminComponent } from "./admin/admin.component";
-import { LoginFormComponent } from "./vue-initiale/login-form/login-form.component";
-import { DialogSimpleComponent } from "./admin/dialog-simple/dialog-simple.component";
-import { DialogMultipleComponent } from "./admin/dialog-multiple/dialog-multiple.component";
-import { ParticlesModule } from "angular-particle";
-import { UserService } from "./vue-initiale/user.service";
-import { PartieSimpleService } from "./admin/partie-simple.service";
-import { ListePartieSimpleComponent } from "./liste-parties/liste-partie-simple/liste-partie-simple.component";
-import { ListePartieMultipleComponent } from "./liste-parties/liste-partie-multiple/liste-partie-multiple.component";
-import { VueMultipleComponent } from "./partie/vue-multiple/vue-multiple.component";
-import { PartieMultipleService } from "./admin/partie-multiple.service";
-import { DialogConfirmationComponent } from "./liste-parties/dialog-confirmation/dialog-confirmation.component";
-import { SocketClientService } from "./socket/socket-client.service";
-import { ListePartieServiceService } from "./liste-parties/liste-partie-service.service";
-import { DialogErreurComponent } from "./admin/dialog-erreur/dialog-erreur.component";
-import { DialogVueAttenteComponent } from "./liste-parties/dialog-vue-attente/dialog-vue-attente.component";
-import { ChatComponent } from "./chat/chat.component";
-import { VueSimpleComponent } from "./partie/vue-simple/vue-simple.component";
-import { DialogFinPartieComponent } from "./partie/dialog-fin-partie/dialog-fin-partie.component";
-import { NotifierService, NotifierModule } from "angular-notifier";
+import {ListePartiesComponent} from "./liste-parties/liste-parties.component";
+import {ChronoComponent} from "./chrono/chrono.component";
+import {HeaderComponent} from "./common/header/header.component";
+import {VueInitialeComponent} from "./vue-initiale/vue-initiale.component";
+import {AdminComponent} from "./admin/admin.component";
+import {LoginFormComponent} from "./vue-initiale/login-form/login-form.component";
+import {DialogSimpleComponent} from "./admin/dialog-simple/dialog-simple.component";
+import {DialogMultipleComponent} from "./admin/dialog-multiple/dialog-multiple.component";
+import {ParticlesModule} from "angular-particle";
+import {UserService} from "./vue-initiale/user.service";
+import {PartieSimpleService} from "./admin/partie-simple.service";
+import {ListePartieSimpleComponent} from "./liste-parties/liste-partie-simple/liste-partie-simple.component";
+import {ListePartieMultipleComponent} from "./liste-parties/liste-partie-multiple/liste-partie-multiple.component";
+import {VueMultipleComponent} from "./partie/vue-multiple/vue-multiple.component";
+import {PartieMultipleService} from "./admin/partie-multiple.service";
+import {DialogConfirmationComponent} from "./liste-parties/dialog-confirmation/dialog-confirmation.component";
+import {SocketClientService} from "./socket/socket-client.service";
+import {ListePartieServiceService} from "./liste-parties/liste-partie-service.service";
+import {DialogErreurComponent} from "./admin/dialog-erreur/dialog-erreur.component";
+import {DialogVueAttenteComponent} from "./liste-parties/dialog-vue-attente/dialog-vue-attente.component";
+import {ChatComponent} from "./chat/chat.component";
+import {VueSimpleComponent} from "./partie/vue-simple/vue-simple.component";
+import {DialogFinPartieComponent} from "./partie/dialog-fin-partie/dialog-fin-partie.component";
+import {NotifierService, NotifierModule} from "angular-notifier";
+import * as Constantes from "./constantes";
 
-const config: SocketIoConfig = { url: "http://localhost:4200", options: {} };
+const config: SocketIoConfig = {url: Constantes.BASE_URL, options: {}};
 
 const appRoutes: Routes = [
-    { path: "", component: VueInitialeComponent },
-    { path: "header", component: HeaderComponent },
-    { path: "admin", component: AdminComponent },
-    { path: "liste-parties", component: ListePartiesComponent },
-    { path: "chrono", component: ChronoComponent },
-    { path: "partie-simple/:idPartie/:channelId", component: VueSimpleComponent },
-    { path: "partie-multiple/:idPartie/:channelId", component: VueMultipleComponent },
+    {path: "", component: VueInitialeComponent},
+    {path: "header", component: HeaderComponent},
+    {path: "admin", component: AdminComponent},
+    {path: "liste-parties", component: ListePartiesComponent},
+    {path: "partie-simple/:idPartie/:channelId", component: VueSimpleComponent},
+    {path: "partie-multiple/:idPartie/:channelId", component: VueMultipleComponent},
 ];
 
 @NgModule({
@@ -126,4 +126,5 @@ const appRoutes: Routes = [
         DialogFinPartieComponent,
     ],
 })
-export class AppModule { }
+export class AppModule {
+}
