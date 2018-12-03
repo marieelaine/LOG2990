0ee24d67c89d57702ed67905cf69d89769367b71
marieelaine <marieelaine@github.com>
2018-12-02 21:28:12 -0500

    modifications de constantes dialog vue attente

--- client/src/app/liste-parties/liste-partie-simple/liste-partie-simple.component.ts
+++ client/src/app/liste-parties/liste-partie-simple/liste-partie-simple.component.ts
@@ -15,7 +15,7 @@ import * as constantes from "../../constantes";
 
 const LARGEUR_BOITE: string = "600px";
 const HAUTEUR_BOITE_190: string = "190px";
-const HAUTEUR_BOITE_220: string = "220px";
+const TAILLE_DIALOG_ATTENTE: string = "280px";
 const URL_PARTIE_SIMPLE: string = "/partie-simple/";
 const URL_SLASH: string = "/";
 
@@ -146,8 +146,8 @@ export class ListePartieSimpleComponent extends ListePartiesComponent implements
 
     private ouvrirDialogVueAttente(partieId: string): void {
         this.dialog.open(DialogVueAttenteComponent, {
-            height: HAUTEUR_BOITE_220,
-            width: LARGEUR_BOITE,
+            height: TAILLE_DIALOG_ATTENTE,
+            width: TAILLE_DIALOG_ATTENTE,
             data: {
                 id: partieId,
                 isSimple: true
